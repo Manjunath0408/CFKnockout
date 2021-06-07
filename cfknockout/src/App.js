f642d848eaed6f3a748136e67b50a77e41000ea2
Manjunath0408 <nath.vasam@gmail.com>
2021-06-07 18:45:37 +0530

    Rooms added(join, create options)

--- cfknockout/src/App.js
+++ cfknockout/src/App.js
@@ -1,10 +1,73 @@
+import { useEffect,useState } from "react";
+import { io } from "socket.io-client";
+
+const axios = require("axios")
+
+const socket = io("http://localhost:4000");
 
 function App() {
+  const [roomData, setRoomData] = useState([]);
+  const [userid, setUserid] = useState("");
+  const [roomid, setRoomid] = useState("");
+  useEffect(() => {
+    var stored_roomid = sessionStorage.getItem("roomid");
+    var stored_userid = sessionStorage.getItem("userid");
+
+     if(stored_roomid && stored_userid){
+      var joiningData = {
+        userid: stored_userid,
+        roomid: stored_roomid,
+        refresh: true
+      }
+       socket.emit("joinRoom", joiningData);
+     }
+  },[])
+  socket.on("updateUsers", () => {
+    axios.get("http://localhost:4000/rooms",{
+      params:{
+        roomid: sessionStorage.getItem("roomid")
+      }
+    })
+    .then(res => {
+      setRoomData(res.data.room.users)
+    })
+    .catch(err => console.log(err))
+  })
+  function handleJoin(){
+    sessionStorage.setItem("userid",userid);
+    sessionStorage.setItem("roomid",roomid);
+    var joiningData = {
+      userid: userid,
+      roomid: roomid,
+      refresh: false
+    }
+    console.log(userid,joiningData)
+    socket.emit("joinRoom",joiningData);
+  }
+  function handleCreate(){
+    sessionStorage.setItem("userid",userid);
+    sessionStorage.setItem("roomid",roomid);
+    var joiningData = {
+      userid: userid,
+      roomid: roomid,
+      refresh: false
+    }
+    socket.emit("joinRoom", joiningData);
+  }
   return (
-    <div className="App">
-     
-    </div>
+    <>
+     <input value={userid} onChange={(e) => setUserid(e.target.value)} placeholder="userid"/>
+     <input value={roomid} onChange={(e) => setRoomid(e.target.value)} placeholder="roomid"/>
+     <button onClick={handleCreate}>Create</button>
+     <button onClick={handleJoin}>JoinRoom</button>
+     {
+       roomData?roomData.map((user) => 
+        <div>{user}</div>
+       ):<div>nothing</div>
+     }
+    </>
   );
 }
 
 export default App;
+ 
