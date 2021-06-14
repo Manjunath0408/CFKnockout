03bfad557aa73af7be2d11013cd51e6528865553
Manjunath0408 <65973346+Manjunath0408@users.noreply.github.com>
2021-06-14 15:02:30 +0530

    Delete client directory

--- client/landingPage.js
+++
@@ -1,9 +0,0 @@
-let login = document.querySelector('#login');
-let password = document.querySelector('#password');
-
-console.log(login);
-console.log(password);
-
-document.querySelector('fadeIn fourth').on
-if (login == "abc" && password=="abc")
-    window.location = 'client/index.html';
